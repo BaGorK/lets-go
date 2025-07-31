f106dcc7a6c57fb2e6168f9c0862497cf9a9b27c
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-07-31 06:20:47 +0300

    main:create events module

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,15 +1,39 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/BaGorK/lets-go/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world from the Gin framework!",
-		})
-	})
+	router.GET("/events", getEvents)
+	router.POST("/events", createEvent)
 
 	router.Run()
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "Event created successfully",
+		"event":   event,
+	})
 }
